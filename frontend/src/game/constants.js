07d925fdccd9c705b784643cbdcaa8035906c404
DrDWatt <89610525+DrDWatt@users.noreply.github.com>
2026-06-02 00:40:32 -0400

    Add frontend/src/game/constants.js

---
+++ frontend/src/game/constants.js
@@ -0,0 +1,8 @@
+export const BOARD_SIZE = 20;
+export const TICK_MS = 120;
+export const INITIAL_DIRECTION = { x: 1, y: 0 };
+export const INITIAL_SNAKE = [
+  { x: 8, y: 10 },
+  { x: 7, y: 10 },
+  { x: 6, y: 10 },
+];
